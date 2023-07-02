af1d1b2343699b5e5f1518b6b0906cd7f95ab929
ayushborage28 <ayushborage28@gmail.com>
2023-07-02 22:57:22 +0530

    minor bug

--- app/listings/[LisitingID]/page.tsx
+++ app/listings/[LisitingID]/page.tsx
@@ -1,4 +1,3 @@
-
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import getListingById from "@/app/actions/getListingbyID";
 import getReservations from "@/app/actions/getreservations";
@@ -6,39 +5,43 @@ import ListingClient from "./ListingClient";
 import ClientOnly from "@/app/components/ClientOnly";
 import EmptyState from "@/app/components/EmptyState";
 
-
-interface IParams{
-    listingID?:string  ;
+interface IParams {
+  listingID?: string;
 }
 
-const ListingPage = async({params}:{params:IParams}) => {
+const ListingPage = async ({ params }: { params: IParams }) => {
+  const listing = await getListingById({ listingID: params.listingID });
 
-const listing = await getListingById(params)
-const reservations = await getReservations(params)
-const currentUser = await getCurrentUser()
-if(!listing){
-    return(
+  const reservations = await getReservations(params);
+  const currentUser = await getCurrentUser();
+  if (!listing) {
+    return (
       <ClientOnly>
-    <EmptyState/>
+        <EmptyState />
       </ClientOnly>
-
-
-    )
+    );
   }
+  if (!currentUser) {
+    return null; // or return a loading state or handle the null case in another way
+  }
+  const formattedUser = {
+    ...currentUser,
+    createdAt: currentUser.createdAt.toString(),
+    updatedAt: currentUser.updatedAt.toString(),
+    emailVerified: currentUser.emailVerified?.toString() || null,
+  };
+  const formattedListing = {
+    ...listing,
+    createdAt: listing.createdAt.toString(),
+    user: formattedUser,
+  };
   return (
     <div>
-
-    <ClientOnly>
-      <ListingClient
-    listing = {listing}
-        currentUser ={currentUser}
-        
-      />
-
-    </ClientOnly>
-
+      <ClientOnly>
+        <ListingClient listing={formattedListing} currentUser={currentUser} />
+      </ClientOnly>
     </div>
-  )
-}
+  );
+};
 
-export default ListingPage
+export default ListingPage;
