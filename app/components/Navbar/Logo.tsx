05514327fdc4498030f15295738d9f13d821ed3a
ayushborage28 <ayushborage28@gmail.com>
2023-07-03 09:50:44 +0530

    add logo optimization

--- app/components/Navbar/Logo.tsx
+++ app/components/Navbar/Logo.tsx
@@ -1,24 +1,19 @@
-'use client';
+"use client";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
-
-
+import Link from "next/link";
 
 const Logo = () => {
-    const router = useRouter();
-
   return (
-<img
-onClick={()=> router.push('/')}
-alt = "Logo"
-className ="hidden md:block cursor-pointer"
-width={150}
-height={150}
-src="/images/logo.png"
-
-/>
-
-  )
-}
+    <Link href="/">
+      <Image
+        src="/images/logo.png"
+        alt="Logo"
+        className="hidden md:block"
+        width={150}
+        height={150}
+      />
+    </Link>
+  );
+};
 
 export default Logo;
